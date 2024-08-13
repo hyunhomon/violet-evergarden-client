723bcc8b6008dba0f0bbea200e1fa7c6383fdf50
hyunhomon <hyunhomon@gmail.com>
2024-08-13 20:38:19 +0900

    re: Chapter

---
+++ src/components/onboard/Chapter.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components/native";
+import React, { useEffect } from "react";
+import Colors from "../../consts/Colors.tsx";
+
+interface ChapterProps {
+  index: number;
+};
+
+const Chapter = ({ index }: ChapterProps) => {
+  return (
+    <ChapterContainer>
+      <ChapterChip $disabled={index !== 1} />
+      <ChapterChip $disabled={index !== 2} />
+      <ChapterChip $disabled={index !== 3} />
+      <ChapterChip $disabled={index !== 4} />
+      <ChapterChip $disabled={index !== 5} />
+    </ChapterContainer>
+  );
+};
+
+const ChapterContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 6px;
+`;
+const ChapterChip = styled.View<{$disabled: boolean}>`
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: ${props => props.$disabled ? Colors.Gray100 : Colors.Gray900};
+`;
+
+export default Chapter;
